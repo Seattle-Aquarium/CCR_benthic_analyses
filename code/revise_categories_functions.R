105b85438f17cadf004d8d5a9537fe88eaa72555
Zachary Randell <z.randell@seattleaquarium.org>
2024-08-07 07:38:26 -0700

    new script w/ functions only

---
+++ code/revise_categories_functions.R
@@ -0,0 +1,95 @@
+## function to remove characters from columns
+remove_characters <- function(df, column_name, direction, num_chars) {
+  trim_chars <- function(x, direction, num_chars) {
+    if (direction == "left") {
+      return(substr(x, num_chars + 1, nchar(x)))
+    } else if (direction == "right") {
+      return(substr(x, 1, nchar(x) - num_chars))
+    } else {
+      stop("Invalid direction. Use 'left' or 'right'.")
+    }
+  }
+  
+  df[[column_name]] <- sapply(df[[column_name]], trim_chars, direction = direction, num_chars = num_chars)
+  return(df)
+}
+
+
+## function to bring new column to the [1] position 
+front.ofthe.line <- function(data){
+  num.col <- ncol(data)
+  data <- data[c(num.col, 1:num.col-1)]
+  return(data)
+}
+
+
+## function to create a unique key identifier combining site and transect info
+create.key <- function(data){
+  data$key <- data$site
+  data$key <- with(data, paste0(key,"_",transect))
+  data <- front.ofthe.line(data)
+  return(data)
+}
+
+
+## function to create 1:nrow SU - sample unit - variable 
+create.SU <- function(data){
+  data$SU <- 1:nrow(data)
+  data <- front.ofthe.line(data)
+  return(data)
+}
+
+
+## copy columns to new dataframe (create metadata df)
+split_dataframe <- function(df, num_cols){
+  new.df <- df[, 1:num_cols]
+  return(new.df)
+}
+
+
+## delete columns, but preserve some
+delete_columns <- function(df, cols_to_delete, cols_to_preserve) {
+  cols_to_delete_names <- names(df)[cols_to_delete]   # Convert column indices to names
+  cols_to_delete_names <- setdiff(cols_to_delete_names, cols_to_preserve)    # Remove the columns to be preserved from the delete list
+  df <- df[, !names(df) %in% cols_to_delete_names]    # Select columns that are not in the delete list
+  return(df)
+}
+
+
+## combine columns - add together the CoralNet counts from multiple categories
+combine_columns <- function(df, columns, new_column_name) {
+  df <- df %>%
+    mutate(!!sym(new_column_name) := rowSums(select(., all_of(columns))))
+  return(df)
+}
+
+
+## delete columns once they have been combined
+remove_columns <- function(df, columns_to_delete) {
+  df <- df %>% select(-all_of(columns_to_delete))
+  return(df)
+}
+
+
+## function that uses previous two functions to edit categories
+revise_categories <- function(data, category_list, new_column){
+  new_category <- category_list
+  data <- combine_columns(data, new_category, new_column)
+  data <- remove_columns(data, new_category)
+  return(data)
+}
+
+
+## function to rename columns
+rename_columns <- function(data, old, new) {
+  names(data)[names(data) %in% old] <- new
+  return(data)
+}
+
+
+## function to save a csv file
+save.csv <- function(data, file.name){
+  setwd(data_output)
+  write.csv(data, file.name, row.names=FALSE)
+}
+## END function definition ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
