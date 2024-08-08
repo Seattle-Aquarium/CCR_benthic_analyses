7b6e0105eafa5cf601bdac1cd6fc980a42f33d48
Zachary Randell <z.randell@seattleaquarium.org>
2024-08-08 07:31:49 -0700

    Update revise_CoralNet_categories.R

--- code/revise_CoralNet_categories.R
+++ code/revise_CoralNet_categories.R
@@ -9,6 +9,7 @@
 ## start up ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## clear working history
 rm(list=ls())
+options(error = NULL)
 
 
 ## add libraries
@@ -38,7 +39,7 @@ setwd(data_input)
 
 
 ## read in csv file 
-dat <- read.csv("combined_csv.csv")
+dat <- read.csv("2022_multiple_transects.csv")
 ## END startup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -47,22 +48,23 @@ dat <- read.csv("combined_csv.csv")
 
 ## invoke functions - data cleaning / processing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## remove "S" and "T" labels from site columns 
-dat <- remove_characters(dat, "site", "left", 1)
-dat <- remove_characters(dat, "transect", "left", 1)
+#dat <- remove_characters(dat, "site", "left", 1)
+#dat <- remove_characters(dat, "transect", "left", 1)
 
 
 ## create key and SU columns, sort data appropriately by site
-dat <- create.key(dat)
-dat <- arrange(dat, site)
+#dat <- create.key(dat)
+#dat <- arrange(dat, site)
 dat <- create.SU(dat)
 
 
 ## split off metadata
-metadata <- split_dataframe(dat, 31)
+metadata <- split_dataframe(dat, 27)
 
 
 ## columns to delete (most metadata)
-cols_to_delete <- 1:31 
+#cols_to_delete <- 1:31
+cols_to_delete <- 1:27
 
 
 ## but preserve these specific columns 
@@ -71,48 +73,69 @@ cols_to_preserve <- c("SU", "key", "site", "transect", "img_name") # Columns to 
 
 ## invoke function
 dat <- delete_columns(dat, cols_to_delete, cols_to_preserve) # Delete columns and get the updated dataframe
+dat$key <- as.factor(dat$key)
 ## END data processing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
 
 
+## summarize data - photos per transect ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+transect_summary <- dat %>%
+  group_by(key) %>% summarise(count = n())
+
+## rename col 
+colnames(transect_summary)[1] <- "transect"
+
+## save summary dat 
+setwd(urban_kelp)
+write.csv(transect_summary, "2022_photos_complete_per_transect.csv", row.names=FALSE)
+## END data summary ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
 ## revise CoralNet categories ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## smooth brown algae categories
-dat <- revise_categories(dat, c("coland_KE", "X5rib_KE", "cabb_KE"), "textured_kelp")
+revised <- dat
 
 ## smooth brown algae categories
-dat <- revise_categories(dat, c("X3rib_KE", "broad_KE", "split_KE", "strap_KE"), "smooth_kelp")
+revised <- revise_categories(revised, c("coland_KE", "X5rib_KE", "cabb_KE"), "textured_kelp")
+
+## smooth brown algae categories
+revised <- revise_categories(revised, c("X3rib_KE", "broad_KE", "split_KE", "strap_KE"), "smooth_kelp")
 
 ## other brown kelp categories
-dat <- revise_categories(dat, c("cord_KE", "DiRet_KE", "feath_KE", "LaEph_KE", 
+revised <- revise_categories(revised, c("cord_KE", "DiRet_KE", "feath_KE", "LaEph_KE", 
                                 "LaLoSi_KE", "palm_KE", "ribbn_KE", "stalk_KE"),"other_brown_kelp")
 
 ## revise red algae categories
-dat <- revise_categories(dat, c("filam_RE", "bushy_RE", "branch_RE", "leaf_RE", "unk_RE"), "red_algae")
+revised <- revise_categories(revised, c("filam_RE", "bushy_RE", "branch_RE", "leaf_RE", "unk_RE"), "red_algae")
 
 ## revise shell categories
-dat <- revise_categories(dat, c("Shell_SU", "lgshell_SU"), "shell_SU")
+revised <- revise_categories(revised, c("shell_SU", "lgshell_SU"), "shell")
 
 ## revise hard substrate categories
-dat <- revise_categories(dat, c("reef_SU", "bould_SU", "concr_SU"), "hard_substrate")
+revised <- revise_categories(revised, c("reef_SU", "bould_SU", "concr_SU"), "hard_substrate")
 
 ## revise hard substrate categories
-dat <- revise_categories(dat, c("glass_SU", "metal_SU", "poly_SU", "wood_SU", "anth_SU"), "anthro_substrate")
+revised <- revise_categories(revised, c("glass_SU", "metal_SU", "poly_SU", "wood_SU", "anth_SU"), "anthro_substrate")
 
 ## revise eelgrass, surfgrass categories
-dat <- revise_categories(dat, c("eel_SG", "surf_SG"), "seagrass")
+revised <- revise_categories(revised, c("eel_SG", "surf_SG"), "seagrass")
 
 ## revise sessile invert categories
-dat <- revise_categories(dat, c("HydBry_SI", "cup_SI", "CucEmb_SI", "mussel_SI", 
+revised <- revise_categories(revised, c("HydBry_SI", "cup_SI", "CucEmb_SI", #"mussel_SI", 
                                 "scallop_SI", "SponSol_SI", "TunSol_SI", "anem_SI", "encru_SI"), "sessile_invert")
 
+
+
 ## revise mobile invert categories
-dat <- revise_categories(dat, c("CaCuc_MS", "crab_MS", "gastro_MS", 
+revised <- revise_categories(revised, c("CaCuc_MS", "crab_MS", "gastro_MS", 
                                 "fish_MS", "SStar_MS", "urchin_MS"), "mobile_invert")
 
 ## revise eelgrass, surfgrass categories
-dat <- revise_categories(dat, c("art_CA", "crust_CA"), "coralline_algae")
+revised <- revise_categories(revised, c("art_CA", "crust_CA"), "coralline_algae")
 ## end category revision ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -120,60 +143,45 @@ dat <- revise_categories(dat, c("art_CA", "crust_CA"), "coralline_algae")
 
 ## renmame columns and double-check ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## print df with names to check against renamed columns
-original_names <- as.data.frame(print(colnames(dat)))
+original_names <- as.data.frame(print(colnames(revised)))
 
 
 ## rename cols
-dat <- rename_columns(dat, "SS_SU", "soft_sediment")
-dat <- rename_columns(dat, "cob_SU", "cobble")
-dat <- rename_columns(dat, "peb_SU", "pebble")
-dat <- rename_columns(dat, "KelpBry_SI", "kelp_bryozoan")
-dat <- rename_columns(dat, "UNIdent", "unknown")
-dat <- rename_columns(dat, "filam_BR", "filamentous_brown")
-dat <- rename_columns(dat, "FlatAci_BR", "acid_weed")
-dat <- rename_columns(dat, "fucus_BR", "rock_weed")
-dat <- rename_columns(dat, "holdfas_BR", "kelp_holdfast")
-dat <- rename_columns(dat, "sargass_BR", "sargassum")
-dat <- rename_columns(dat, "sugar_KE", "sugar_kelp")
-dat <- rename_columns(dat, "shell_SU", "shell_debris")
-dat <- rename_columns(dat, "ulva_GR", "green_algae")
+revised <- rename_columns(revised, "SS_SU", "soft_sediment")
+revised <- rename_columns(revised, "cob_SU", "cobble")
+revised <- rename_columns(revised, "peb_SU", "pebble")
+revised <- rename_columns(revised, "KelpBry_SI", "kelp_bryozoan")
+revised <- rename_columns(revised, "UNIdent", "unknown")
+revised <- rename_columns(revised, "filam_BR", "filamentous_brown")
+revised <- rename_columns(revised, "FlatAci_BR", "acid_weed")
+revised <- rename_columns(revised, "fucus_BR", "rock_weed")
+revised <- rename_columns(revised, "holdfas_BR", "kelp_holdfast")
+revised <- rename_columns(revised, "sargass_BR", "sargassum")
+revised <- rename_columns(revised, "sugar_KE", "sugar_kelp")
+revised <- rename_columns(revised, "shell", "shell_debris")
+revised <- rename_columns(revised, "ulva_GR", "green_algae")
 
 
 ## print revised names
-revised_names <- as.data.frame(print(colnames(dat)))
+revised_names <- as.data.frame(print(colnames(revised)))
 combined <- cbind(original_names, revised_names)
 
 
 ## delete extraneous columns
-dat <- remove_columns(dat, c("StriAci_BR", "undaria_BR", "unk_BR", "giant_KE", 
+revised <- remove_columns(revised, c("StriAci_BR", "undaria_BR", "unk_BR", "giant_KE", 
                              "bullBL_KE", "bullST_KE", "senes_RE", "senes_AL"))
 
 
 ## remove columns with very few observations
-dat <- remove_columns(dat, c("rock_weed", "acid_weed", "smooth_kelp", "seagrass"))
-## END CoralNet category editing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+revised <- remove_columns(revised, c("rock_weed", "acid_weed", "smooth_kelp", "seagrass"))
 
 
+## remove extra telemetry log columns
+revised <- remove_columns(revised, c("alt_smooth", "SimDIS"))
 
 
-
-## export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-setwd(urban_kelp)
-write.csv(dat, "2022_T1_revised_categories.csv", row.names=FALSE)
-## END export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-
-## sample down to 50pts per image ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## run function - sample 50 data points without replacement
-dat2 <- sample.data(dat, 50, FALSE)
-
-
-## delete the SU column, no longer needed
-dat2 <- dat2[,-1]
+## trim dfs
+remove(original_names, revised_names)
 
 
 ## new ordering of columns
@@ -200,13 +208,36 @@ new.order <- c("key", "site", "transect", "img_name",
 
 
 ## Reorder the columns
-dat2 <- dat2 %>% select(all_of(new.order))
+revised <- revised %>% select(all_of(new.order))
 
 
 ## as.factor for future plotting
-dat2$site <- as.factor(dat2$site)
-dat2$transect <- as.factor(dat2$transect)
-dat2$key <- as.factor(dat2$key)
+revised$site <- as.factor(revised$site)
+revised$transect <- as.factor(revised$transect)
+revised$key <- as.factor(revised$key)
+## END CoralNet category editing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+## export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+setwd(urban_kelp)
+write.csv(revised, "2022_all_current_photos.csv", row.names=FALSE)
+## END export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+## sample down to 50pts per image ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## run function - sample 50 data points without replacement
+dat2 <- sample.data(dat, 50, FALSE)
+
+
+## delete the SU column, no longer needed
+dat2 <- dat2[,-1]
 
 
 ## save the data frame 
