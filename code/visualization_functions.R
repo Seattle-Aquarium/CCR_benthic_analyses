8c3b316c2faa567796746634f003d004379ee06b
Zachary Randell <z.randell@seattleaquarium.org>
2024-08-08 09:50:51 -0700

    tidy up script

--- code/visualization_functions.R
+++ code/visualization_functions.R
@@ -1,30 +1,6 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Data visualizations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Data visualizations functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-## start up ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## clear working history
-rm(list=ls())
-
-
-## add libraries
-library(tidyverse)
-
-
-## list out current path
-getwd()
-
-
-## hardcode relative file paths
-code <- "../code"
-data_input <- "../data_input"
-data_output <- "../data_output"
-figs <- "../figs"
-## END startup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
@@ -59,6 +35,11 @@ mult.theme = theme(panel.grid.major = element_blank(),
                    )
 
 
+## transect colors
+cols <- scale_color_manual(values=c("#B22222", "#009ACD"))
+fills <- scale_fill_manual(values=c("#B22222", "#009ACD"))
+
+
 ## function to create custom graphing window
 my.window <- function(width, height){
   graphics.off()
@@ -77,15 +58,31 @@ pairwise.plot <- function(data, x, y, x.lab, y.lab){
 }
 
 
+
+facet.site <- function(data, x, y, x.lab, y.lab){
+  fig <- ggplot(data=data, aes(x, y)) + my.theme + coord_fixed() + 
+    geom_point(size=1.75, alpha=0.65, aes(color=transect)) + cols +
+    #geom_smooth(method = "loess", se = FALSE, color = "red") +
+    facet_wrap(~site, ncol=4) +
+    xlab(x.lab) + ylab(y.lab) + xlim(0,100) + ylim(0,100) #+
+    #theme(legend.position = "none")
+  return(fig)
+}
+
+
+
+
+
 ## function for modified pairwise plot for "n" pairwise comparisons
 mult.pairwise.plot <- function(data, x, y, x.lab, y.lab) {
   ggplot(data, aes_string(x = x, y = y)) + 
     mult.theme + coord_fixed() + 
-    geom_point(size = 1, alpha = 0.5, aes(color=site)) +
+    geom_point(size = 1, alpha = 0.5)+#, aes(color=site)) +
     #geom_smooth(method = "loess", se = FALSE, color = "red") +
     xlab(x.lab) + ylab(y.lab) + xlim(0, 100) + ylim(0, 100) +
     theme(legend.position = "none")
 }
+
 
 
 ## function to save both pdf and png files of each fig 
@@ -93,6 +90,16 @@ save.figs <- function(pdf, png, plot.lab, width.x, height.y){
   setwd(figs)
   ggsave(pdf, plot = plot.lab, width = width.x, height = height.y)
   ggsave(png, plot = plot.lab, width = width.x, height = height.y)
+}
+
+
+## sample down to 50 rows (or n rows)
+sample.data <- function(data, nrows, replace.logic){
+  new.dat <- data %>%
+    group_by(key) %>%
+    sample_n(size = nrows, replace = replace.logic) %>%
+    ungroup()
+  return(new.dat)
 }
 ## END graphing params & graphing functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
