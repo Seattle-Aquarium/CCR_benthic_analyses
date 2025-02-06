bd11d8d5e1aae93a778485bbaed89f17f587178c
Zachary Randell <randellz@seaq.priv>
2025-02-06 11:56:53 -0800

    Update wrangle_data.R

--- code/wrangle_data.R
+++ code/wrangle_data.R
@@ -1,5 +1,7 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## CoralNet data processing and category editing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## wrangle data to key columns, calculate percent-cover, reduce to key columns ~
+## 2025_02_06, zhr  
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -26,7 +28,8 @@ code <- "code"
 data_input <- "data_input"
 data_output <- "data_output"
 figs <- "figs"
-urban_kelp <- "data_output/urban_kelp"
+#urban_kelp <- "data_output/urban_kelp"
+active <- "data_output/active"
 
 
 ## source functions 
@@ -50,6 +53,7 @@ dat <- create.SU(dat)
 
 ## Columns to preserve
 cols_to_preserve <- c("SU", "key", "site", "transect", "img_name") # Columns to preserve
+cols_to_remove <- c("alt_smooth", "SimDIS")
 
 
 ## Invoke the function to delete columns between "SU" and "Points", but preserve certain columns
@@ -57,7 +61,6 @@ dat <- delete_columns(dat, "SU", "Points", cols_to_preserve)
 
 
 ## specific columns to remove, if present
-cols_to_remove <- c("alt_smooth", "SimDIS")
 dat <- dat[, !names(dat) %in% cols_to_remove]
 ## END metadata cleanup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -66,10 +69,6 @@ dat <- dat[, !names(dat) %in% cols_to_remove]
 
 
 ## invoke function to calculate percent-cover ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-dat$pt_sum <- rowSums(select(dat, cup_SI:surf_SG))
-dat <- front.ofthe.line(dat)
-
-
 #dat <- calculate.percent(dat, soft_sediment, mobile_invert, 2)
 dat <- calculate.percent(dat, cup_SI, surf_SG, 2)
 
@@ -77,109 +76,51 @@ dat <- calculate.percent(dat, cup_SI, surf_SG, 2)
 ## add total percent column at front of data.frame - check that sums to 1 
 dat$sum <- rowSums(select(dat, cup_SI:surf_SG))
 dat <- front.ofthe.line(dat)
-
-
-## function to calculate the average across columns and round to 2 decimal places
-avg_all <- calculate.avg(dat, "soft_sediment", "mobile_invert")
-avg_all <- reconstruct.metadata(avg_all)
-
-
-## open or save csv 
-# dat <- read.csv(file.path(urban_kelp, "2022_all_current_photos.csv"))
-# write.csv(revised, file.path(urban_kelp, "2022_all_current_photos.csv"), row.names=FALSE)
-# write.csv(avg_all, file.path(urban_kelp, "2022_avg_all.csv"), row.names=FALSE)
-## END export and revised data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## END percent-cover calculation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
 
 
-## pull in and bind bull kelp stipes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-stipes <- read.csv(file.path(data_input, "bull_kelp_stipes.csv"))
-stipes <- create.key(stipes)
+## reduce to key transects of interest ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+T3.1 <- dat %>% filter(transect %in% c("1", "2", "3"))
+count_rows(T3.1, "key")
 
 
-## bind stipes to avg_all based on key
-avg_all <- bind.stipes(avg_all, stipes)
+## sample down to 50 per transect, or retain all rows if nrow <50
+T3.2 <- sample.down(T3.1, "key", 50)
+count_rows(test.T3, "key")
 
 
-## save csv w/ bull kelp stipes and category averages
-#write.csv(avg_all, file.path(urban_kelp, "2022_avg_all.csv"), row.names=FALSE)
-## END bull kelp stipes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## only retain transects with at least 50 photos. 
+T3.3 <- sample.down.filtered(T3, "key", 50)
+count_rows(second.test.T3, "key")
+
+
+## test to make sure the sampling didn't generate any repeat rows
+duplicates <- T3.3 %>% count(SU) %>% filter(n > 1)
+
+
+## save relevant files ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#write.csv(T3, file.path(active, "2022_all_three_transects_and_rows.csv"), row.names=FALSE)
+#write.csv(T3.2, file.path(active, "2022_sampled_down_to_50.csv"), row.names=FALSE)
+#write.csv(T3.3, file.path(active, "2022_50_minimum_or_drop.csv"), row.names=FALSE)
+## END row sampling and file save ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
 
-## filter and sample down to 50pts per image ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#T1_T2 <- revised %>% filter(transect %in% c("1", "2"))
-#count_rows(T1_T2, "key")
+## calculate category sums and save as separate files ~~~~~~~~~~~~~~~~~~~~~~~ ##
+T3.1_totals <- annotation.sum(T3.1, "cup_SI", "surf_SG")
+T3.2_totals <- annotation.sum(T3.2, "cup_SI", "surf_SG")
+T3.3_totals <- annotation.sum(T3.3, "cup_SI", "surf_SG")
 
 
-## sample down to 50 per transect
-#T1_T2 <- sample.data(T1_T2, 50, FALSE)
-#count_rows(T1_T2, "key")
-
-
-## calculate category sums and save 
-# sum_T1_T2 <- annotation.sum(T1_T2, "soft_sediment", "mobile_invert")
-# write.csv(sum_T1_T2, file.path(urban_kelp, "sum_2022_T1_T2.csv"))
-
-
-## save the data frame 
-# T1_T2 <- read.csv(file.path(urban_kelp, "2022_T1_T2.csv"))
-# write.csv(T1_T2, file.path(urban_kelp, "2022_T1_T2.csv"), row.names = FALSE)
-
-
-## calculate and save photo list
-# T1_T2_list <- photo.list(T1_T2)
-# write.csv(T1_T2_list, file.path(urban_kelp, "2022_T1_T2_photos.csv"), row.names=FALSE)
-
-
-## filter down to T3 
-T3 <- dat %>% filter(transect %in% c("1", "2", "3"))
-
-
-## sample 50 rows, or however many present, for T1, T2, and T3 
-row_min <- 50
-T3 <- sample.down(T3, "key", row_min)
-
-
-## test how many rows are present - double check 
-count_rows(T3, "key")
-
-
-## function to calculate the average across columns and round to 2 decimal places
-avg_T3 <- calculate.avg(T3, "soft_sediment", "mobile_invert")
-avg_T3 <- reconstruct.metadata(avg_T3)
-
-
-## read in stipes
-#stipes <- read.csv(file.path(data_input, "bull_kelp_stipes.csv"))
-#stipes <- create.key(stipes)
-
-
-## bind stipes
-avg_T3 <- bind.stipes(avg_T3, stipes)
-
-
-## save avg'd data
-#write.csv(avg_T3, file.path(urban_kelp, "2022_avg_T1_T2_T3.csv"), row.names=FALSE)
-
-
-## calculate category sums and save 
-# sum_T1_T2_T3 <- annotation.sum(T1_T2_T3, "soft_sediment", "mobile_invert")
-# write.csv(sum_T1_T2_T3, file.path(urban_kelp, "sum_2022_T1_T2_T3.csv"))
-
-
-## save or read the data
-# T1_T2_T3 <- read.csv(file.path(urban_kelp, "2022_T1_T2_T3.csv"))
-# write.csv(T1_T2_T3, file.path(urban_kelp, "2022_T1_T2_T3.csv"), row.names = FALSE)
-
-
-## calculate and save photo list
-#T1_T2_T3_list <- photo.list(T1_T2_T3)
-#write.csv(T1_T2_T3_list, file.path(urban_kelp, "2022_T1_T2_T3_photos.csv"), row.names = FALSE)
-## end data filtering and csv export ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## save csvs 
+write.csv(T3.1_totals, file.path(active, "T3-1_69_labels_totals.csv"), row.names=FALSE)
+write.csv(T3.2_totals, file.path(active, "T3-2_69_labels_totals.csv"), row.names=FALSE)
+write.csv(T3.3_totals, file.path(active, "T3-3_69_labels_totals.csv"), row.names=FALSE)
+## END category sums ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 
 
