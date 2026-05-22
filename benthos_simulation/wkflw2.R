aa1d50cd6bf764b2a343f12e9f295db1dfede6e3
rachaelaber <aberr@oregonstate.edu>
2026-05-22 09:02:34 -0700

    clean wkflw2

--- benthos_simulation/wkflw2.R
+++ benthos_simulation/wkflw2.R
@@ -39,14 +39,14 @@ newdat <- data.frame(
   depth_c = seq(min(cp$depth_c), max(cp$depth_c), length.out = 200),
   location = rep(as.factor("Centennial_Park"), times = 200)
 )
-newdat$pred <- predict(fit_sk_sq, newdata = newdat, type = "response", re.form = NA)
+newdat$pred <- predict(fit_sk_sq, newdata = newdat, type = "response", re.form = NA) # assume r.e. 0
 
 ggplot(cp, aes(depth_c, sugar)) +
   geom_point() +
   geom_line(data = newdat, aes(depth_c, pred), color = "green") +
-  geom_smooth(method = "loess", color = "blue")
+  geom_smooth(method = "loess", color = "blue") # note this line does not correspond to any of the above models
 
-# Make sk predictions for points in TIF; project
+# Make sk predictions for points in TIF
 r <- rast('data/Extract_Cent_1.tif')
 print(r)
 plot(r)
@@ -68,10 +68,10 @@ ggplot(pts, aes(col = pred)) +
   labs(title = "Sugar kelp", col = "Prediction") +
   theme_bw()
 
-# Project and create transects poly
+# Create transects poly
 transects <- read.csv('data/perpendicular_transects.csv') %>%
   select(Transect_key, Latitude, Longitude, Area_m2, Width) %>%
-  mutate(length = Area_m2 / Width)
+  mutate(length = Area_m2/Width)
 transects <- st_as_sf( 
   transects,
   coords = c("Longitude", "Latitude"),
@@ -101,32 +101,5 @@ ggplot(pts) +
   ) +
   theme_bw()
   
-# Compute overlap; krig
+# Compute overlap
 pts_in_poly <- st_filter(pts, transects_poly)
-pts_in_poly_sp <- as(pts_in_poly, "Spatial")
-vgm_emp <- variogram(pred ~ depth_c, pts_in_poly_sp)
-plot(vgm_emp)
-vgm_fit <- fit.variogram(
-  vgm_emp,
-  model = vgm(psill = 0.02, model = "Sph", range = 100, nugget = 0.001)
-)
-plot(vgm_emp, vgm_fit)
-
-pts_sp <- as(pts, "Spatial")
-kriged <- krige(
-  formula  = pred ~ depth_c,
-  locations = pts_in_poly_sp,
-  newdata   = pts_sp,
-  model     = vgm_fit,
-  nmax = 50,     # local kriging neighborhood
-  maxdist = 500  # ignore distant points
-)
-pts$pred_krig <- kriged$var1.pred
-pts$pred_var  <- kriged$var1.var
-
-ggplot(pts, aes(col = pred)) +
-  geom_sf()
-
-ggplot(pts, aes(col = pred_krig)) +
-  geom_sf()
-  
